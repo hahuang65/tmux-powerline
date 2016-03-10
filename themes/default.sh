d7c7ad7d87f434358eef1a7f93b41cc7ec243b06
Howard Huang <hao@hwrd.me>
2016-03-09 19:50:39 -0800

    Rearrange Tmux segments

--- themes/default.sh
+++ themes/default.sh
@@ -48,13 +48,13 @@ if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
 		#"tmux_session_info ${YELLOW} ${BKGND}" \
 		"hostname ${BLUE} ${BKGND}" \
+		"load ${PURPLE} ${BKGND}" \
 		# "ifstat 30 255" \
 		# "ifstat_sys 30 255" \
 		# "lan_ip ${AQUA} ${BKGND} ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
 		# "wan_ip ${AQUA} ${BKGND}" \
 		# Change git_colour="236" in the vcs_branch.sh segment to the same color.
-    "kiex ${AQUA} ${BKGND} ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
-    "rvm ${AQUA} ${BKGND}"
+		"pwd ${AQUA} ${BKGND}" \
 		"vcs_branch ${GREEN} ${BKGND}" \
 		"vcs_compare 60 255" \
 		"vcs_staged 64 255" \
@@ -66,20 +66,20 @@ fi
 if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
 		# "earthquake 3 0" \
-		"pwd ${BLUE} ${BKGND}" \
 		"mailcount ${ORANGE} ${BKGND}" \
 		"now_playing ${YELLOW} ${BKGND}" \
+    "kiex ${AQUA} ${BKGND}" \
+    "rvm ${AQUA} ${BKGND} ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
 		# "cpu 240 136" \
-		"load ${PURPLE} ${BKGND}" \
 		# "tmux_mem_cpu_load 234 136" \
 		# Change the low battery foreground color in the battery.sh segment code.
 		"battery ${RED} ${BKGND}" \
 		# "weather 37 255" \
 		# "rainbarf 0 0" \
 		# "xkb_layout 125 117" \
-		"date_day ${BROWN} ${BKGND}" \
-		"date ${BROWN} ${BKGND} ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
-		"time ${BROWN} ${BKGND} ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		"date_day ${GREEN} ${BKGND}" \
+		"date ${GREEN} ${BKGND} ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		"time ${GREEN} ${BKGND} ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
 		# "utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
 	)
 fi
